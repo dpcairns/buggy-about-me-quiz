26c7adaad1be37ea9f3e4f1c4ad6ea38fafda258
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-14 17:59:26 -0800

    got guessesRemaining countdown working

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 const button = document.getElementById('button');
 const guessesRemaining = document.getElementById('guessesRemaining');
 let userNumber = document.getElementById('userNumber');
-
 const correctNumber = 13;
 let count = 4;
 
 // function to compare correctNumber to userNumber
-function (compareNumbers) {
+function compareNumbers() {
     if (userNumber === correctNumber) {
-        return 'Congratulations! You guessed the number!'
+        return 'Congratulations! You guessed the number!';
     } else if (userNumber > correctNumber) {
-        return 'Your guess was too high--try again!'
+        return 'Your guess was too high--try again!';
     } else if (userNumber < correctNumber) {
-        'Your guess was too low--try again!'
-    };
+        return 'Your guess was too low--try again!';
+    }
 };
 
 // function carried out on click. Needs to 1. Reduce counter; 2. Test the number; 3. Show result
-addEventListener('click', () => {
+button.addEventListener('click', () => {
+    console.log('Button works!');
     count--;
-    compareNumbers;
+    compareNumbers();
+    guessesRemaining.textContent = count;
 })
-
-
-
-
-
-
-
-guessesRemaining.textContent = count

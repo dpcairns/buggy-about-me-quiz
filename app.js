67f0e6074dc88586626d5ef541570714db8eff06
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-14 17:40:19 -0800

    define variables from DOM

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const button = document.getElementById('button');
 const guessesRemaining = document.getElementById('guessesRemaining');
 let userNumber = document.getElementById('userNumber');
 
@@ -5,19 +6,20 @@ const correctNumber = 13;
 let count = 4;
 
 // function to compare correctNumber to userNumber
-compareNumbers() => {
-    if userNumber === correctNumber {
+function (compareNumbers) {
+    if (userNumber === correctNumber) {
         return 'Congratulations! You guessed the number!'
-    } else if userNumber > correctNumber {
+    } else if (userNumber > correctNumber) {
         return 'Your guess was too high--try again!'
-    } else if userNumber < correctNumber {
+    } else if (userNumber < correctNumber) {
         'Your guess was too low--try again!'
-    }
+    };
 };
 
 // function carried out on click. Needs to 1. Reduce counter; 2. Test the number; 3. Show result
 addEventListener('click', () => {
     count--;
+    compareNumbers;
 })
 
 

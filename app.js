69e3c4b5f366e3ae8c2d8aa5ee703987011c9da7
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-14 17:25:44 -0800

    add html and beginnings of functions

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-// import isYes function
-import isYes from './isyes.js';
+const guessesRemaining = document.getElementById('guessesRemaining');
+let userNumber = document.getElementById('userNumber');
 
-// define variables
-const takeQuiz = document.getElementById('takeQuiz');
-const total = document.getElementById('total');
+const correctNumber = 13;
+let count = 4;
 
-// function for Take Quiz button
-takeQuiz.addEventListener('click', () => {
-    console.log('Button works!');
-    const name = prompt('Enough about me. What is your name?');
-    const confirmation = confirm(`${name}, would you like to take a short quiz?`);
-    if (confirmation === false) return; 
-    const from = prompt('Enough about you. Is Chelsea from Minnesota?');
-    const likes = prompt('Does Chelsea like to party?');
-    const bones = prompt('Has Chelsea broken her back?');
-    let count = 0;
-    if (!isYes(from)) count++;
-    if (!isYes(likes)) count++;
-    if (isYes(bones)) count++;
-    total.textContent = `Congratulations ${name}, you got ${count} answers correct!`;
-    scoreStyle(count);
-});
-
-//stretch goal; make page display differently based on good/bad scores
-function scoreStyle(count) {
-    const total = document.getElementById('total');
-    if (count > 2) {
-        total.style.backgroundColor = 'yellow';
-    } else {
-        total.style.backgroundColor = 'purple';
-        total.style.color = 'white'
+// function to compare correctNumber to userNumber
+compareNumbers() => {
+    if userNumber === correctNumber {
+        return 'Congratulations! You guessed the number!'
+    } else if userNumber > correctNumber {
+        return 'Your guess was too high--try again!'
+    } else if userNumber < correctNumber {
+        'Your guess was too low--try again!'
     }
 };
+
+// function carried out on click. Needs to 1. Reduce counter; 2. Test the number; 3. Show result
+addEventListener('click', () => {
+    count--;
+})
+
+
+
+
+
+
+
+guessesRemaining.textContent = count
--- isyes.js
+++
@@ -1,8 +0,0 @@
-// export isYes function
-export default function isYes(str) {
-    if (str.toLowerCase() === 'yes' || str.toLowerCase() === 'y') return true; else return false;
-};
-
-
-// instructions The quiz will be asking yes/no questions, but receiving a text response from the user. They could response "Yes", "y", "yes", "Y", etc.
-// TDD an `isYes` function that that returns `true` for the responses you want to be "yes" (otherwise it should return `false`). Write a test for each "yes" text that your app will accept. Then write a couple tests for common "no" responses. 

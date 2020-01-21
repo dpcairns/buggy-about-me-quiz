106242240785fa60d4c3ecbb1bbac4d723b25764
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-21 11:10:44 -0800

    objects now center regardless of screen size!

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const total = document.getElementById('total');
 
 // function for Take Quiz button
 takeQuiz.addEventListener('click', () => {
-    console.log('Button works!');
     const name = prompt('Enough about me. What is your name?');
     const confirmation = confirm(`${name}, would you like to take a short quiz?`);
     if (confirmation === false) return; 
     const from = prompt('Enough about you. Is Chelsea from Minnesota?');
-    const likes = prompt('Does Chelsea like to party?');
+    const likes = prompt('Does Chelsea like to go dancing?');
     const bones = prompt('Has Chelsea broken her back?');
     let count = 0;
     if (!isYes(from)) count++;
@@ -24,11 +23,10 @@ takeQuiz.addEventListener('click', () => {
 
 //stretch goal; make page display differently based on good/bad scores
 function scoreStyle(count) {
-    const total = document.getElementById('total');
     if (count > 2) {
         total.style.backgroundColor = 'yellow';
     } else {
         total.style.backgroundColor = 'purple';
-        total.style.color = 'white'
+        total.style.color = 'white';
     }
-};
+}
